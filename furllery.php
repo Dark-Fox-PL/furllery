603d9e2862359654163634e6ddc5281cf90f96e8
Kamil Kmetyk <kamil.kmetyk@darkfox.pl>
2024-03-08 00:25:02 +0100

    Scrollbar fix.

--- furllery.php
+++ furllery.php
@@ -8,7 +8,7 @@ Plugin Name: Furllery
 Plugin URI: https://darkfox.pl
 Description: A WordPress gallery plugin with a bit of fur on it!
 Author: Reyn with 🧡 and 🍄
-Version: 1.5.2
+Version: 1.5.3
 Author URI: https://darkfox.pl
 */
 
